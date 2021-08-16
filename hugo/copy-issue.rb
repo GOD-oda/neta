499902f70fab7da86a9423a67584d206848ce06a
Workman <GOD-oda@users.noreply.github.com>
2021-08-17 02:57:49 +0900

    Replace title and draft (#66)

--- hugo/copy-issue.rb
+++ hugo/copy-issue.rb
@@ -13,10 +13,17 @@ uri = URI.parse("https://api.github.com/repos/GOD-oda/neta/issues/#{issue_id}")
 
 res = Net::HTTP.get_response(uri)
 res_body = JSON.parse(res.body)
+title = res_body['title']
+body = res_body['body']
+file_path = "posts/#{issue_id}.md"
 
-%x(hugo new posts/#{issue_id}.md)
+%x(hugo new #{file_path})
 
-File.open("content/posts/#{issue_id}.md", 'a') do |f|
+system("sed -i -e \"s/title\:.*/title\: #{title}/\" content/#{file_path}")
+system("sed -i -e \"s/draft\:.*/draft: false/\" content/#{file_path}")
+system("rm content/#{file_path}-e")
+
+File.open("content/#{file_path}", 'a') do |f|
   f.puts res_body['body']
 end
 
