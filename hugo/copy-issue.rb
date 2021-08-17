00b239d19b7ab47820ab48a83a1e11c086505c6c
t.oda <takahiro.tech.oda@gmail.com>
2021-08-18 02:26:58 +0900

    Add created_at and updated_at

--- hugo/copy-issue.rb
+++ hugo/copy-issue.rb
@@ -2,6 +2,7 @@
 
 require 'net/http'
 require 'json'
+require 'time'
 
 issue_id = ARGV[0]
 if issue_id.nil? || issue_id.to_i < 1
@@ -15,18 +16,39 @@ res = Net::HTTP.get_response(uri)
 res_body = JSON.parse(res.body)
 title = res_body['title']
 body = res_body['body']
-file_path = "posts/#{issue_id}.md"
+file_name = "#{issue_id}.md"
+file_path = "content/posts/#{file_name}"
 label_names = res_body['labels'].map { |l| l['name'] }.join(',')
-
-%x(hugo new #{file_path})
-
-system("sed -i -e \"s/title\:.*/title\: #{title}/\" content/#{file_path}")
-system("sed -i -e \"s/draft\:.*/draft: false/\" content/#{file_path}")
-unless label_names.empty?
-  system("gsed -i -e \"/draft\:.*/a tags: [#{label_names}]\" content/#{file_path}")
+if res_body['created_at']
+  ENV['TZ'] = 'Asia/Tokyo'
+  created_at = Time.parse(res_body['created_at'])
+  created_at = created_at + (60 * 60 * 9)
+else
+  created_at = nil
+end
+if res_body['updated_at']
+  ENV['TZ'] = 'Asia/Tokyo'
+  updated_at = Time.parse(res_body['updated_at'])
+  updated_at = updated_at + (60 * 60 * 9)
+else
+  updated_at = nil
 end
 
-File.open("content/#{file_path}", 'a') do |f|
+%x(hugo new posts/#{file_name})
+
+system("sed -i -e \"s/title\:.*/title\: #{title}/\" #{file_path}")
+system("sed -i -e \"s/draft\:.*/draft: false/\" #{file_path}")
+unless label_names.empty?
+  system("gsed -i -e \"/draft\:.*/a tags: [#{label_names}]\" #{file_path}")
+end
+if created_at
+  system("sed -i -e \"s/date\:.*/date: #{created_at.strftime('%Y-%m-%dT%H:%M:%S')}/\" #{file_path}")
+end
+if updated_at
+  system("gsed -i -e \"/date\:.*/a publishDate: #{updated_at.strftime('%Y-%m-%dT%H:%M:%S')}\" #{file_path}")
+end
+
+File.open(file_path, 'a') do |f|
   f.puts res_body['body']
 end
 
